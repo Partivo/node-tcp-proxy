9bdc929b60af0e0c097719e4201907537ea1a08d
Fantros <krostotr@gmail.com>
2022-11-28 20:59:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,16 @@ export default class tcpProxy {
 		this.options.listen.host = this.options.listen.host || '127.0.0.1';
 		this.options.listen.port = this.options.listen.port || this.target.split(":")[1];
 		net.createServer((socket) => {
-			var client = tcpProxy.createProxy(socket, this.target);
+			var client = tcpProxy.createProxy(socket, {
+				target: this.target
+			});
 			this.#log(socket, client);
 			socket.on('close', () => client.end());
 		}).listen(this.options.listen.port, this.options.listen.host);
 	}
 
-	static createProxy(socket, target) {
-		target = target.split(":");
+	static createProxy(socket, options) {
+		const target = options.target.split(":");
 		const client = net.createConnection(target[1], target[0]);
 		client.on('close', () => socket.end());
 
