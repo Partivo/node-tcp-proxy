a7e9a25ac76555f2712f58dbbbba68a789fc9e30
Fantros <krostotr@gmail.com>
2022-11-29 14:19:37 +0300

    Update cli.js

--- cli.js
+++ cli.js
@@ -10,20 +10,14 @@ if(!myArgs[0] && !myArgs[1]) {
 	process.exit(1);
 }
 
-const options = {
-	log: (data) => {
-		if(data.type == "access") console.info(JSON.stringify(data.log));
-		else if(data.type == "error") console.error(JSON.stringify(data.log));
-		else console.warn(data);
-	}
-};
-
 if(myArgs[2] == "-l" || myArgs[2] == "-listen") options.listen = {
 	host: myArgs[3],
 	port: myArgs[4]
 };
 
 var proxy = new tcpProxy(`${myArgs[0]}:${myArgs[1]}`, options);
+proxy.on('access', (data) => console.info(data));
+proxy.on('error', (data) => console.error(data));
 
 process.on("uncaughtException", (err) => {
 	console.error(err);
