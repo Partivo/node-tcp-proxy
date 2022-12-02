317a73e1ebd865ef3d85de01ebf66715808a67e3
Fantros <krostotr@gmail.com>
2022-12-02 13:56:56 +0300

    Update cli.js

--- cli.js
+++ cli.js
@@ -3,26 +3,25 @@ const tcpProxy = require('./index.js');
 
 const myArgs = process.argv.slice(2);
 
-if(!myArgs[0] && !myArgs[1]) {
+if(!myArgs[0]) {
 	console.log('Upstream IP or Port not found!');
-	console.log('Example: tcp-proxy 127.0.0.1 8080');
-	console.log('Optional: tcp-proxy 127.0.0.1 8080 -l 127.0.0.1 8888');
+	console.log('Example: tcp-proxy 127.0.0.1:8080');
+	console.log('Optional: tcp-proxy 127.0.0.1:8080 -l 127.0.0.1:8888');
 	process.exit(1);
 }
 
 const options = {};
-if(myArgs[2] == "-l" || myArgs[2] == "-listen") options.listen = {
-	host: myArgs[3],
-	port: myArgs[4]
-};
+if(myArgs[1] == "-l" || myArgs[1] == "-listen") options.listen = myArgs[2];
 
-var proxy = new tcpProxy(`${myArgs[0]}:${myArgs[1]}`, options);
+var proxy = new tcpProxy(myArgs[0], options);
 proxy.on('access', (data) => console.info(data));
 proxy.on('error', (data) => console.error(data));
 
-process.on("uncaughtException", (err) => {
+process.on("uncaughtException", function(err) {
 	console.error(err);
 	proxy.end();
 });
 
-process.on("SIGINT", () => proxy.end());
+process.on("SIGINT", function() {
+	proxy.end();
+});
