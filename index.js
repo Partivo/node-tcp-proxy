b6bb6fe44fdf43c2b686a452f2ac6e50baa727ba
Fantros <krostotr@gmail.com>
2022-12-14 14:24:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,80 @@
-import { EventEmitter } from 'events';
-import net from 'net';
+const { EventEmitter } = require('events');
+const net = require('net');
 
-export default class tcpProxy {
-    constructor(target, options) {
-        this.eventEmitter = new EventEmitter();
 
-        this.target = target;
-        this.options = options;
-        this.#createServer();
+function tcpProxy(target, options) {
+    this.eventEmitter = new EventEmitter();
 
-        return this.eventEmitter;
-    }
+    this.target = target;
+    this.options = options;
+    this.createServer();
 
-    static createProxy(socket, options) {
-        const proxyEmitter = new EventEmitter();
-        const target = options.target.split(":");
+    return this.eventEmitter;
+}
 
-        const client = net.createConnection(target[1], target[0]);
-        client.on('close', () => socket.end());
+tcpProxy.createProxy = function(socket, options) {
+    const proxyEmitter = new EventEmitter();
+    const target = options.target.split(":");
 
-        socket.pipe(client);
-        client.pipe(socket);
+    const client = net.createConnection(target[1], target[0]);
+    client.on('close', function() {
+        socket.end();
+    });
 
-        proxyEmitter.on('end', () => client.end());
-        client.on("error", (err) => proxyEmitter.emit('error', err));
+    socket.pipe(client);
+    client.pipe(socket);
 
-        return proxyEmitter;
-    }
+    proxyEmitter.on('end', function() {
+        client.end();
+    });
+    client.on("error", function(err) {
+        proxyEmitter.emit('error', err);
+    });
 
-    #createServer() {
-        this.options.listen = this.options.listen.split(":");
-        net.createServer((socket) => {
-            const client = tcpProxy.createProxy(socket, {
-                target: this.target
-            });
-            this.#log(socket, client);
-            socket.on('close', () => client.emit('end'));
-        }).listen(this.options.listen[1], this.options.listen[0]);
-    }
+    return proxyEmitter;
+}
 
-    #log(socket, client) {
-        // Access
-        this.eventEmitter.emit('access', {
-            message: 'connect',
-            remoteAddress: socket.remoteAddress
+tcpProxy.prototype.createServer = function() {
+    this.options.listen = this.options.listen.split(":");
+    net.createServer((socket) => {
+        const client = tcpProxy.createProxy(socket, {
+            target: this.target
         });
-        socket.on('end', () => this.eventEmitter.emit('access', {
+        this.log(socket, client);
+        socket.on('close', function() {
+            client.emit('end');
+        });
+    }).listen(this.options.listen[1], this.options.listen[0]);
+}
+
+tcpProxy.prototype.log = function(socket, client) {
+    // Access
+    this.eventEmitter.emit('access', {
+        message: 'connect',
+        remoteAddress: socket.remoteAddress
+    });
+    socket.on('end', function() {
+        this.eventEmitter.emit('access', {
             message: "disconnect",
             remoteAddress: socket.remoteAddress
-        }));
+        });
+    });
 
-        // Error
-        socket.on("error", (err) => this.eventEmitter.emit('error', {
+    // Error
+    socket.on("error", function(err) {
+        this.eventEmitter.emit('error', {
             message: "server",
             remoteAddress: socket.remoteAddress,
             error: err
-        }));
-        client.on("error", (err) => this.eventEmitter.emit('error', {
+        });
+    });
+    client.on("error", function(err) {
+        this.eventEmitter.emit('error', {
             message: "upstream",
             remoteAddress: socket.remoteAddress,
             error: err
-        }));
-    }
+        });
+    });
 }
+
+module.exports = tcpProxy;
